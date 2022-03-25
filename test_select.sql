68fe71e2966eae711c59c38c0f4469f928b0c2c1
leha5345 <leha5345@gmail.com>
2022-03-25 17:58:56 +0300

    add sql

--- test_select.sql
+++ test_select.sql
@@ -1,16 +1,10 @@
+-----------------------
 
 
-select t.name as tablename, c.name as fieldname
-from sys.columns c
-join sys.tables t on t.object_id=c.object_id
-where t.name='StockItems'
-ORDER BY 2
+-------------
 
 select * 
 	from Warehouse.StockItems
 WHERE
 	StockItemName LIKE '%urgent%'
 	OR StockItemName LIKE 'Animal%'
-
-
-	---------------
